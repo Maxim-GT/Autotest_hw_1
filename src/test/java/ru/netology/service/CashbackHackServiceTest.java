a0489e647bdf7067e6f197fbb0a2f9199f6a52da
Maxim Glukhov <glukhov_maxim@mail.ru>
2022-09-24 12:14:32 +0700

    Testng added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainUnderLimit() {
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainEqualsLimit() {
+        int expected = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainOverLimit() {
+        int expected = 999;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1001);
+        assertEquals(actual, expected);
+    }
+}
